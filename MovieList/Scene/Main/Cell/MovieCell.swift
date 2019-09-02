9a99ad242b89886e306d17a970ca90bead1b8c00
Nanthicha K <nanthicha13@gmail.com>
2019-09-02 18:15:04 +0700

    Create MovieCell

---
+++ MovieList/Scene/Main/Cell/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  MovieList
+//
+//  Created by Nanthicha Kritveeranant on 2/9/2562 BE.
+//  Copyright © 2562 Nanthicha Kritveeranant. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
